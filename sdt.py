dbe43046f4559c91e6973de192a16104ee05705c
Pascal Kühner <kuehner@incoretex.de>
2020-12-02 19:55:38 +0100

    pushed into class

--- sdt.py
+++ sdt.py
@@ -1,38 +1,131 @@
 import pm4py
 from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
 
-event_log = pm4py.read_xes("/home/pascal/Downloads/running-example.xes")
-dfg = dfg_discovery.apply(event_log)
-sdt = {}
-frequencies = {}
-
-# Find frequencies of each one
-for trace in event_log:
-    for event in trace:
-        event_name = event['concept:name']
-        if event_name not in frequencies:
-            frequencies[event_name] = 0
-        frequencies[event_name] += 1
-
-# Build standard sdt
-for event_a in frequencies:
-    for event_b in frequencies:
-        df_counter = dfg[(event_a, event_b)]
-        if event_b not in sdt:
-            sdt[event_b] = {}
-        sdt[event_b][event_a] = df_counter / frequencies[event_a]
-
-#print(sdt)
 
 
-def find_concurrent_activities(sdt, act_a):
-    conc = [act_a]
-    for event_b in frequencies:
-        if act_a != event_b:
-            if sdt[event_b][act_a] != 0 and sdt[act_a][event_b] != 0:
-                conc.append(event_b)
+class StandardDependencyTable:
+    """A simple example class"""
+    def __init__(self, event_log):
+        self.event_log = event_log
+        self.dfg = dfg_discovery.apply(event_log)
+        self.sdt = {}
+        self.frequencies = {}
+        self.concs = {}
+        self.find_frequencies()
+        self.find_standard_sdt()
+        self.find_and_mark_concurrents()
+        self.build_final_sdt()
 
-    return conc
+
+    def find_frequencies(self):
+        for trace in self.event_log:
+            for event in trace:
+                event_name = event['concept:name']
+                if event_name not in self.frequencies:
+                    self.frequencies[event_name] = 0
+                self.frequencies[event_name] += 1
+
+    def find_standard_sdt(self):
+        for event_a in self.frequencies:
+            for event_b in self.frequencies:
+                df_counter = self.dfg[(event_a, event_b)]
+                if event_b not in self.sdt:
+                    self.sdt[event_b] = {}
+                self.sdt[event_b][event_a] = df_counter / self.frequencies[event_a]
+
+    def find_and_mark_concurrents(self):
+        for event_a in self.frequencies:
+            self.concs[event_a] = self.find_concurrent_activities(event_a)
+            for event_b in self.concs[event_a]:
+                if event_b != event_a:
+                    self.sdt[event_a][event_b] = -1
+                    self.sdt[event_b][event_a] = -1
+
+    def find_concurrent_activities(self, act_a):
+        conc = [act_a]
+        for event_b in self.frequencies:
+            if act_a != event_b:
+                if self.sdt[event_b][act_a] != 0 and self.sdt[act_a][event_b] != 0:
+                    conc.append(event_b)
+
+        return conc
+
+    def build_final_sdt(self):
+        for event_a in self.frequencies:
+            for event_c in self.concs[event_a]:
+                df_counter = 0
+                for event_b in self.frequencies:
+                    df_2 = self.dfg[(event_a, event_b)]
+                    if df_2 > 0 and event_b not in self.concs[event_a]:
+                        df_counter += self.dfg[(event_c, event_b)]
+                for event_b in self.frequencies:
+                    if df_counter != 0:
+                        self.sdt[event_b][event_c] = self.dfg[(event_c, event_b)] / df_counter
+
+    def print_std(self):
+        for line in self.sdt:
+            print(line)
+            print(self.sdt[line])
+
+    def check_mutual_exclusion(self):
+        pws = powerset(list(self.frequencies.keys()))
+        for event_a in self.frequencies:
+            freq = self.frequencies[event_a]
+            for set in pws:
+                if event_a in set:
+                    continue
+                works = True
+                if len(set) > 1:
+                    dfg_sum = 0
+                    sdt_sum = 0
+                    for event in set:
+                        if self.dfg[(event_a, event)] != 0:
+                            # dfg_sum += dfg[(event_a, event)]
+                            dfg_sum += self.frequencies[event]
+                            sdt_sum += self.sdt[event][event_a]
+                        else:
+                            works = False
+                            break
+                    if not works:
+                        continue
+                    if dfg_sum == freq and sdt_sum == 1:
+                        print("Found Mut Excl")
+                        print(set)
+                        print(event_a)
+
+    def check_mutual_exclusion_join(self):
+        pws = powerset(list(self.frequencies.keys()))
+        for event_a in self.frequencies:
+            freq = self.frequencies[event_a]
+            for set in pws:
+                if event_a in set:
+                    continue
+                works = True
+                if len(set) > 1:
+                    dfg_sum = 0
+                    sdt_sum = 0
+                    for event in set:
+                        if self.dfg[(event, event_a)] != 0:
+                            # dfg_sum += dfg[(event, event_a)]
+                            dfg_sum += self.frequencies[event]
+                            sdt_sum += self.sdt[event_a][event]
+                        else:
+                            works = False
+                            break
+                    if not works:
+                        continue
+                    if dfg_sum == freq and sdt_sum == 1:
+                        print("Found Mut Excl Join")
+                        print(set)
+                        print(event_a)
+
+    def check_sequence(self):
+        for event_a in self.frequencies:
+            for event_b in self.frequencies:
+                if event_b != event_a and self.sdt[event_b][event_a] == 1 and self.frequencies[event_a] == self.dfg[
+                    (event_a, event_b)]:
+                    print(event_a + ' --> ' + event_b)
+
 
 #Creates powerset without y
 def powerset(s):
@@ -42,96 +135,13 @@ def powerset(s):
         powerset.append([s[j] for j in range(x) if (i & (1 << j))])
     return powerset
 
-
-def check_mutual_exclusion(sdt, dfg):
-    pws = powerset(list(frequencies.keys()))
-    for event_a in frequencies:
-        freq = frequencies[event_a]
-        for set in pws:
-            if event_a in set:
-                continue
-            works = True
-            if len(set) > 1:
-                dfg_sum = 0
-                sdt_sum = 0
-                for event in set:
-                    if dfg[(event_a, event)] != 0:
-                        #dfg_sum += dfg[(event_a, event)]
-                        dfg_sum += frequencies[event]
-                        sdt_sum += sdt[event][event_a]
-                    else:
-                        works = False
-                        break
-                if not works:
-                    continue
-                if dfg_sum == freq and sdt_sum == 1:
-                    print("Found Mut Excl")
-                    print(set)
-                    print(event_a)
-
-def check_mutual_exclusion_join(sdt, dfg):
-    pws = powerset(list(frequencies.keys()))
-    for event_a in frequencies:
-        freq = frequencies[event_a]
-        for set in pws:
-            if event_a in set:
-                continue
-            works = True
-            if len(set) > 1:
-                dfg_sum = 0
-                sdt_sum = 0
-                for event in set:
-                    if dfg[(event, event_a)] != 0:
-                        #dfg_sum += dfg[(event, event_a)]
-                        dfg_sum += frequencies[event]
-                        sdt_sum += sdt[event_a][event]
-                    else:
-                        works = False
-                        break
-                if not works:
-                    continue
-                if dfg_sum == freq and sdt_sum == 1:
-                    print("Found Mut Excl Join")
-                    print(set)
-                    print(event_a)
+eventLog = pm4py.read_xes("/home/pascal/Downloads/running-example.xes")
+table = StandardDependencyTable(eventLog)
+table.print_std()
+table.check_mutual_exclusion()
+table.check_sequence()
+table.check_mutual_exclusion_join()
 
 
-def check_sequence(sdt, dfg):
-    for event_a in frequencies:
-        for event_b in frequencies:
-            if event_b != event_a and sdt[event_b][event_a] == 1 and frequencies[event_a] == dfg[(event_a, event_b)]:
-                print(event_a + ' --> ' + event_b)
 
 
-concs = {}
-for event_a in frequencies:
-    concs[event_a] = find_concurrent_activities(sdt, event_a)
-    for event_b in concs[event_a]:
-        if event_b != event_a:
-            sdt[event_a][event_b] = -1
-            sdt[event_b][event_a] = -1
-
-for event_a in frequencies:
-    for event_c in concs[event_a]:
-        df_counter = dfg[(event_a, event_b)]
-        if df_counter > 0 and event_b not in concs[event_a]:
-            df_counter = 0
-            for event_b in frequencies:
-                df_counter += dfg[(event_c, event_b)]
-        for event_b in frequencies:
-            if df_counter != 0:
-                sdt[event_b][event_c] = dfg[(event_c, event_b)] / df_counter
-
-for line in sdt:
-    print(line)
-    print(sdt[line])
-print(dfg)
-
-check_sequence(sdt, dfg)
-check_mutual_exclusion(sdt, dfg)
-check_mutual_exclusion_join(sdt, dfg)
-
-from pm4py.objects.conversion.dfg import converter as dfg_mining
-
-net, im, fm = dfg_mining.apply(dfg)
-pm4py.view_petri_net(net, im, fm)
